961164c87119df4e6474a61631c1f3ac63f8a9f5
Al Munnings <al.munnings@gmail.com>
2022-09-19 17:51:49 +1000

    Remove lagoon tools reliance

--- assets/IconAgencyCommands.php
+++ assets/IconAgencyCommands.php
@@ -2,6 +2,7 @@
 
 namespace Drush\Commands\iconagency;
 
+use Symfony\Component\Yaml\Yaml;
 use Consolidation\SiteAlias\SiteAliasManagerAwareTrait;
 use Drush\Attributes as CLI;
 use Drush\Boot\DrupalBootLevels;
@@ -141,12 +142,20 @@ final class IconAgencyCommands extends DrushCommands implements SiteAliasManager
    * Enable Stage File Proxy.
    */
   private function enableStageFileProxy(): void {
-    if (!$url = $this->getProductionUrl()) {
+    if (!$domain = $this->getProductionDomain()) {
       return;
     }
 
+    if ($auth = $this->getProductionAuth()) {
+      $domain = $auth . '@' . $domain;
+    }
+
     $this->run('pm:enable', ['stage_file_proxy']);
-    $this->run('config:set', ['stage_file_proxy.settings', 'origin', $url]);
+    $this->run('config:set', [
+      'stage_file_proxy.settings',
+      'origin',
+      'https://' . $domain,
+    ]);
   }
 
   /**
@@ -186,19 +195,51 @@ final class IconAgencyCommands extends DrushCommands implements SiteAliasManager
   /**
    * Get the Lagoon production URL.
    */
-  private function getProductionUrl(): ?string {
-    /** @var \Drush\SiteAlias\ProcessManager $manager */
-    $manager = $this->processManager();
+  private function getProductionDomain(): ?string {
 
-    $process = $manager->process([
-      'bash',
-      '-c',
-      '. /lagoon-tools/tasks/common/get_production_url && echo $(get_production_url)',
-    ]);
+    $branch = getenv('LAGOON_PRODUCTION_BRANCH') ?: 'master';
+    $project = getenv('LAGOON_PROJECT');
+    $stack = getenv('LAGOON_KUBERNETES');
 
-    $process->run();
+    $yml = Yaml::parse(file_get_contents('/app/.lagoon.yml'));
 
-    return $process->isSuccessful() ? trim($process->getOutput()) : (getenv('LAGOON_PRODUCTION_URL') ?: NULL);
+    $routes = array_reduce($yml['environments']['master']['routes'] ?? [], 'array_merge', []);
+    $domains = array_reduce($routes['nginx'] ?? [], 'array_merge', []);
+    $domains_tls = array_filter($domains, fn($domain) => $domain['tls-acme'] ?? FALSE);
+
+    // If we have a domain and tls is enabled, first value is assumed live.
+    return empty($domains_tls) ? "nginx.$branch.$project.$stack" : key($domains_tls);
+  }
+
+  /**
+   * Get the Lagoon production URL basic auth.
+   */
+  private function getProductionAuth(): ?string {
+
+    $branch = getenv('LAGOON_PRODUCTION_BRANCH') ?: 'master';
+
+    $env_files = array_filter([
+      "/app/.env",
+      "/app/.lagoon.env",
+      "/app/.lagoon.env.$branch",
+    ], 'file_exists');
+
+    $env = [];
+
+    foreach ($env_files as $env_file) {
+      $lines = file($env_file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+      $lines = array_filter($lines, fn($line) => strpos(trim($line), '#') !== 0);
+      foreach ($lines as $line) {
+        [$name, $value] = array_map(fn($v) => trim($v), explode('=', $line, 2));
+        $env[$name] = $value;
+      }
+    }
+
+    $auth = $env['BASIC_AUTH'] ?? "off";
+    $user = $env['BASIC_AUTH_USERNAME'] ?? NULL;
+    $pass = $env['BASIC_AUTH_PASSWORD'] ?? NULL;
+
+    return (strtolower($auth) !== "off" && $user && $pass) ? "$user:$pass" : NULL;
   }
 
   /**
